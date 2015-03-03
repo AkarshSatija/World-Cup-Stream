521ee0ee5f17f98a3b0dea3715f621bce7d82250
Akarsh Satija <akarsh.satija@gmail.com>
2015-03-04 00:36:28 +0530

    Load more limit to 60 records on page by removing old records

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,7 +6,11 @@ app.controller('StreamController', function($scope, $resource) {
 
     $scope.load=function () {
     	console.log("loading");
-        $resource("/stream").query({}, function(data) {
+        $resource("/stream?"+(new Date().getTime())).query({}, function(data) {
+        	
+        	if($scope.stream.length>50)
+        		$scope.stream=$scope.stream.slice(20,$scope.stream.length); // will keep $scope.stream limited so as to prevent heavy data binding. as whole repeat set in binded again on update
+
             $scope.stream = $scope.stream.concat(data);
             console.log(data);
         });
