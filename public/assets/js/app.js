cfcba23ae732915e3d6431f5d295ccb37b975545
Akarsh Satija <akarsh.satija@gmail.com>
2015-03-04 01:15:18 +0530

    segregating

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,39 +1,36 @@
-var app = angular.module('Twitter', ['ngResource','ngClipboard']);
+var app = angular.module('Twitter', ['ngResource', 'ngClipboard']);
 
 app.controller('StreamController', function($scope, $resource) {
     $scope.stream = [];
 
-
-    $scope.load=function () {
-    	console.log("loading");
-        $resource("/stream?"+(new Date().getTime())).query({}, function(data) {
-        	
-        	if($scope.stream.length>50)
-        		$scope.stream=$scope.stream.slice(20,$scope.stream.length); // will keep $scope.stream limited so as to prevent heavy data binding. as whole repeat set in binded again on update
+    $scope.load = function() {
+        console.log("loading");
+        $resource("/stream?" + (new Date().getTime())).query({}, function(data) {
+            if ($scope.stream.length > 50)
+                $scope.stream = $scope.stream.slice(20, $scope.stream.length); // will keep $scope.stream limited so as to prevent heavy data binding. as whole repeat set in binded again on update
 
             $scope.stream = $scope.stream.concat(data);
-            console.log(data);
         });
-
     }
 
-	$scope.fallback = function(copy) {
-      window.prompt('Press cmd+c to copy the text below.', copy);
+    $scope.fallback = function(copy) {
+        window.prompt('Press cmd+c to copy the text below.', copy);
     };
 
     $scope.showMessage = function() {
-      console.log("clip-click works!");
+        console.log("clip-click works!");
     };
 
-$scope.load();
+    $scope.load();
 });
 
+// creating a filter for showing data in reverse order.
 app.filter('reverse', function() {
-  return function(items) {
-    return items.slice().reverse();
-  };
+    return function(items) {
+        return items.slice().reverse();
+    };
 });
 
 app.config(['ngClipProvider', function(ngClipProvider) {
     ngClipProvider.setPath("//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.6/ZeroClipboard.swf");
-  }]);
+}]);
