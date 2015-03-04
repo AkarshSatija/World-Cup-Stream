3e31452a26906c18a5b481309355bf8312606d51
Akarsh Satija <akarsh.satija@gmail.com>
2015-03-04 22:38:42 +0530

    tweet stream trimmed without removing old tweets

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,14 +2,22 @@ var app = angular.module('Twitter', ['ngResource', 'ngClipboard']);
 
 app.controller('StreamController', function($scope, $resource) {
     $scope.stream = [];
+    $scope.stream_trim = [];
 
     $scope.load = function() {
         console.log("loading");
+        $scope.loadButtonText = "Loading...";
         $resource("/stream?" + (new Date().getTime())).query({}, function(data) {
-            if ($scope.stream.length > 50)
-                $scope.stream = $scope.stream.slice(20, $scope.stream.length); // will keep $scope.stream limited so as to prevent heavy data binding. as whole repeat set in binded again on update
+            if ($scope.stream.length > 500) {
+                $scope.stream_trim = $scope.stream_trim.concat($scope.stream.slice(0, 200));
+                console.log("trimmed");
+                $scope.stream = $scope.stream.slice(200, $scope.stream.length); // will keep $scope.stream limited so as to prevent heavy data binding. as whole repeat set in binded again on update
+                $scope.dataTrimmed = false;
+            }
 
             $scope.stream = $scope.stream.concat(data);
+
+            $scope.loadButtonText = "Load More";
         });
     }
 
@@ -20,8 +28,19 @@ app.controller('StreamController', function($scope, $resource) {
     $scope.showMessage = function() {
         console.log("clip-click works!");
     };
+    $scope.loadTrimmed = function() {
 
+        $scope.stream=$scope.stream_trim.concat($scope.stream);
+
+
+    }
+
+
+    $scope.loadButtonText = "Load More";
     $scope.load();
+    $scope.dataTrimmed = true;
+    $scope.scrolledToLast = false;
+    $scope.boolChangeClass = true;
 });
 
 // creating a filter for showing data in reverse order.
@@ -34,3 +53,28 @@ app.filter('reverse', function() {
 app.config(['ngClipProvider', function(ngClipProvider) {
     ngClipProvider.setPath("//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.6/ZeroClipboard.swf");
 }]);
+
+
+
+app.directive("scroll", function($window) {
+    return function(scope, element, attrs) {
+        angular.element($window).bind("scroll", function() {
+
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) // if at last of a page
+            {
+                //trigger loading new posts 
+                // scope.$load();
+
+            }
+
+            if (this.pageYOffset <= 100) {
+                //scope.boolChangeClass = false;
+                //trigger back data if hidden
+                console.log('Header is in view.');
+                /*scope.boolChangeClass = true;
+                console.log('Scrolled below header.');*/
+            }
+            scope.$apply();
+        });
+    };
+});
